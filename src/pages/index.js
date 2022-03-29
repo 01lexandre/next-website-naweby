44ef9dc49d00602e89c51c532c74788cee457118
01lexandre <01.lexandre@gmail.com>
2022-03-29 12:18:25 -0300

    hello world

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="#">Naweby</a>
+          Bem ao Mundo <a href="#">Naweby</a>
         </h1>
       </main>
     </div>
