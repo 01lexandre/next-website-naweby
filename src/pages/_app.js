d36956f58296c6d3edbdf7677b5503ffd53000f3
01lexandre <01.lexandre@gmail.com>
2022-03-31 17:13:01 -0300

    90%

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,19 +5,44 @@ import GlobalStyle from '../styles/global'
 import themeShema from '../styles/theme'
 
 import {ChakraProvider, extendTheme} from '@chakra-ui/react'
+import {AnimatePresence} from "framer-motion";
 
 const theme = extendTheme(themeShema)
+
+// The handler to smoothly scroll the element into view
+const handExitComplete = () => {
+  if (typeof window !== 'undefined') {
+    // Get the hash from the url
+    const hashId = window.location.hash;
+
+    if (hashId) {
+      // Use the hash to find the first element with that id
+      const element = document.querySelector(hashId);
+
+      if (element) {
+        // Smooth scroll to that elment
+        element.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+          inline: 'nearest',
+        });
+      }
+    }
+  }
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <ChakraProvider theme={theme}>
-        <Head>
-          <title>My new cool app</title>
-        </Head>
-        <Component {...pageProps}/>
-        <GlobalStyle />
-      </ChakraProvider>
+      <AnimatePresence exitBeforeEnter onExitComplete={handExitComplete}>
+        <ChakraProvider theme={theme}>
+          <Head>
+            <title>My new cool app</title>
+          </Head>
+          <Component {...pageProps}/>
+          <GlobalStyle />
+        </ChakraProvider>
+      </AnimatePresence>
     </ThemeProvider>
   )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -39,7 +39,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8"/>
           <meta name="robots" content="index, follow"/>
-          <link rel="icon" href="https://www.freenfe.com.br/favicon.ico"/>
+          <link rel="icon" href="/favicon.ico"/>
 
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
         </Head>
