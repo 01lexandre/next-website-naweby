dcfcc4f1c0c8432a74ca9886ddb0acd55574b5bf
01lexandre <01.lexandre@gmail.com>
2022-07-14 11:37:56 -0300

    Ajust google analicts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -45,12 +45,13 @@ function MyApp({ Component, pageProps }) {
           <DefaultSeo {...SEO} />
           <Script
             id="google-analytics"
-            onLoad={() => {
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MTM3DKX');
+              })(window,document,'script','dataLayer','GTM-MTM3DKX');`,
             }}
           />
           <Script
